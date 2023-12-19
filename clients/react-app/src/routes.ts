db51015e1280e8db1dd01b8b2719722ad030932d
hector-crean <hectorcrean@gmail.com>
2023-12-19 10:05:07 +0000

    routes

--- clients/react-app/src/routes.ts
+++ clients/react-app/src/routes.ts
@@ -6,8 +6,8 @@ import "./App.css";
 
 import { VideoGalleryPage } from "@/pages/VideoGalleryPage";
 import { ContentsPage } from "./pages/ContentsPage";
+import { SandboxPage } from "./pages/SandboxPage";
 import { VideoPage } from "./pages/VideoPage";
-
 
 
 /**
@@ -20,6 +20,7 @@ const routes = [
     { path: "/", component: ContentsPage },
     { path: "/videos", component: VideoGalleryPage },
     { path: "/videos/:video_id", component: VideoPage },
+    { path: '/sandbox', component: SandboxPage}
 ] as const;
 
 
